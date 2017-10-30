331a02c13b9aa3f7f4e61c607f450d57689bd72a
chao1991528 <392318709@qq.com>
2017-10-30 18:04:46 +0800

    10.30

--- application/admin/controller/Record.php
+++ application/admin/controller/Record.php
@@ -39,5 +39,14 @@ class Record extends AdminController {
         //还有日志操作undo
         return $res?$this->resMes(200):$this->resMes(400);
     }
+    
+    public function doExport(){
+        $name = '肌肤测试结果'. date("Y-m-d");
+        $records = db('record')->field('skin_type,skin_color,is_guomin,skin_problem,want_solve_problem,is_huli,result_skin_type,result_skin_feature,result_protect_point,create_time')
+                              ->order('id desc') 
+                              ->select();
+        $header = ['皮肤类型', '皮肤颜色', '是否过敏', '皮肤问题', '最想解决的问题', '是否护理过皮肤', '测试结果肌肤属性', '测试结果表现特征', '测试结果保养重点', '创建时间'];
+        excelExport($name, $header, $records);
+    }
 
 }
